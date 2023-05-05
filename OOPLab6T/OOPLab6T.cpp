847630560678cf5ecc811f2ba5b51174a4af0467
Miracle179 <prsky.ivan@gmail.com>
2023-05-05 22:27:39 +0200

    Створено абстрактний базовий клас Figure, який має віртуальну функцію area() для обчислення площі фігури. Далі створено чотири похідних класи: Rectangle, Circle, RightTriangle, Trapezoid. У кожному похідному класі перевизначено функцію area() для обчислення площі фігури в залежності від її форми. У функції main() створено об'єкти цих класів та масив вказівників на базовий клас Figure. Об'єкти похідних класів передаються в цей масив, тому що вони наслідують функціональність базового класу Figure. Далі виконується цикл, який виводить площу кожної фігури в масиві, використовуючи віртуальну функцію area().

--- OOPLab6T/OOPLab6T.cpp
+++ OOPLab6T/OOPLab6T.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <cmath>
 using namespace std;
 
 // Base class without virtual inheritance
@@ -47,7 +48,64 @@ public:
     }
 };
 
+
+
+// Abstract class "Figure"
+class Figure {
+public:
+    virtual double area() = 0; // Virtual area calculation function
+};
+
+// Derivative class "Rectangle"
+class Rectangle : public Figure {
+private:
+    double width;
+    double height;
+public:
+    Rectangle(double w, double h) : width(w), height(h) {}
+    double area() {
+        return width * height;
+    }
+};
+
+// Derivative class "Circle"
+class Circle : public Figure {
+private:
+    double radius;
+public:
+    Circle(double r) : radius(r) {}
+    double area() {
+        return M_PI * radius * radius;
+    }
+};
+
+// Derivative class "Right triangle"
+class RightTriangle : public Figure {
+private:
+    double base;
+    double height;
+public:
+    RightTriangle(double b, double h) : base(b), height(h) {}
+    double area() {
+        return base * height / 2.0;
+    }
+};
+
+// Derivative class "Trapeze"
+class Trapezoid : public Figure {
+private:
+    double base1;
+    double base2;
+    double height;
+public:
+    Trapezoid(double b1, double b2, double h) : base1(b1), base2(b2), height(h) {}
+    double area() {
+        return (base1 + base2) * height / 2.0;
+    }
+};
+
 int main() {
+
     Animal a(5);
     Dog d(3, "Labrador");
     VirtualAnimal va(10);
@@ -57,6 +115,21 @@ int main() {
     cout << "Size of Dog object: " << sizeof(d) << endl;
     cout << "Size of VirtualAnimal object: " << sizeof(va) << endl;
     cout << "Size of VirtualDog object: " << sizeof(vd) << endl;
+
+
+    // Creation of objects of derived classes
+    Rectangle r(5, 10);
+    Circle c(4);
+    RightTriangle t(3, 4);
+    Trapezoid z(3, 6, 5);
+
+    // Creating an array of pointers to the base class
+    Figure* shapes[] = {&r, &c, &t, &z};
+
+    // Derivation of the areas of each figure
+    for (int i = 0; i < 4; i++) {
+        cout << "Shape " << i + 1 << " area = " << shapes[i]->area() << endl;
+    }
 
     return 0;
 }
