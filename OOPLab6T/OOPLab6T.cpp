3fc9fe4ce9d6ec813721130a63ae69f081132876
Miracle179 <prsky.ivan@gmail.com>
2023-05-05 22:14:50 +0200

    Створено дві ієрархії класів: одну з успадкуванням без віртуального успадкування (клас Animal та його похідний клас Dog) та іншу з віртуальним успадкуванням (клас VirtualAnimal та його похідний клас VirtualDog).
    У кожному класі є поля даних та методи для їх відображення. У класі Animal та VirtualAnimal полями є age та weight, а у класі Dog та VirtualDog - breed та name. Клас Dog перевизначає метод display(), щоб відображати не тільки вік, але й породу собаки.
    
    У функції main() створюються об'єкти кожного класу, а потім виводяться їх розміри за допомогою оператора sizeof. Розмір об'єкта визначається кількістю байтів, які він займає в пам'яті.

--- OOPLab6T/OOPLab6T.cpp
+++ OOPLab6T/OOPLab6T.cpp
@@ -1,20 +1,62 @@
-﻿// OOPLab3Tpl.cpp : Цей файл містить є шаблоном для 
-// виконання лаб. роботи №6. 
-// Він містинь функцію "main" з якої починається та закінчується виконання програми.
-//
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
+// Base class without virtual inheritance
+class Animal {
+protected:
+    int age;
+public:
+    Animal(int a = 0) : age(a) {}
+    void display() {
+        cout << "Age: " << age << endl;
+    }
+};
 
-// Ваші файли загловки 
-//
-#include "Lab6Example.h"
-int main()
-{
-    std::cout << " Lab #6  !\n";
-    //  Код виконання завдань
-    //  Головне меню завдань
-    //  Функції та класи можуть знаходитись в інших файлах проекту
+// Derived class without virtual inheritance
+class Dog : public Animal {
+private:
+    string breed;
+public:
+    Dog(int a = 0, string b = "") : Animal(a), breed(b) {}
+    void display() {
+        cout << "Age: " << age << endl;
+        cout << "Breed: " << breed << endl;
+    }
+};
 
-    Example();
+// Base class with virtual inheritance
+class VirtualAnimal {
+protected:
+    int weight;
+public:
+    VirtualAnimal(int w = 0) : weight(w) {}
+    virtual void display() {
+        cout << "Weight: " << weight << endl;
+    }
+};
 
+// Derived class with virtual inheritance
+class VirtualDog : virtual public VirtualAnimal {
+private:
+    string name;
+public:
+    VirtualDog(int w = 0, string n = "") : VirtualAnimal(w), name(n) {}
+    void display() {
+        cout << "Weight: " << weight << endl;
+        cout << "Name: " << name << endl;
+    }
+};
+
+int main() {
+    Animal a(5);
+    Dog d(3, "Labrador");
+    VirtualAnimal va(10);
+    VirtualDog vd(20, "Rex");
+
+    cout << "Size of Animal object: " << sizeof(a) << endl;
+    cout << "Size of Dog object: " << sizeof(d) << endl;
+    cout << "Size of VirtualAnimal object: " << sizeof(va) << endl;
+    cout << "Size of VirtualDog object: " << sizeof(vd) << endl;
+
+    return 0;
 }
